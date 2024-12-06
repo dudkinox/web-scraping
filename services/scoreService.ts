93eca7d0bba13f57d6e9615b5fb6b6705921e357
Guitar Comeback <guitar@kon.in.th>
2024-12-06 10:58:00 +0700

    poc

--- services/scoreService.ts
+++ services/scoreService.ts
@@ -4,80 +4,34 @@ import puppeteer, { Page } from "puppeteer";
 export const getScore = async (req: Request, res: Response) => {
   console.log(`getScore start time ${new Date().toISOString()}`);
 
+  const url =
+    "https://shopee.co.th/product/569947420/27769670284?d_id=ff462&uls_trackid=51e3a6v9000p&utm_content=2ZZ871dT3FpH3YubTnNsDpu5Jh6P";
+
   try {
     const browser = await puppeteer.launch({ headless: "new" });
     const page = await browser.newPage();
-    await page.goto("https://sport.trueid.net/premier-league/tables");
+    await page.goto(url);
 
     const response: any = [];
 
-    for (let i = 2; i < 22; i++) {
-      var team = await page.waitForSelector(
-        `#__next > main > div.sc-aXZVg.korseV.shelfContent > div > div > div > ul > div:nth-child(${i}) > div > div.col-5.team-name.col-team-name > div > div.text-team-name.truncate`
-      );
-      var teamName = await page.evaluate(
-        (element) => element?.textContent,
-        team
-      );
-      var pi = await page.waitForSelector(
-        `#__next > main > div.sc-aXZVg.fyChqS.tablescoresMobile > div > ul > div:nth-child(${i}) > div > div:nth-child(5)`
-      );
-      var scorePI = await page.evaluate((element) => element?.textContent, pi);
+    var stock = await page.waitForSelector(
+      `#sll2-normal-pdp-main > div > div.KrtGbA > div > div.container > section.flex.card.jNRWxO > section._OguPS > div.flex.flex-column > div.TMw1ot > div > div.kkBEei`
+    );
+    var stockValue = await page.evaluate(
+      (element) => element?.textContent,
+      stock
+    );
 
-      var Win = await page.waitForSelector(
-        `#__next > main > div.sc-aXZVg.fyChqS.tablescoresMobile > div > ul > div:nth-child(${i}) > div > div:nth-child(6)`
-      );
-      var scoreWin = await page.evaluate(
-        (element) => element?.textContent,
-        Win
-      );
-
-      var Draw = await page.waitForSelector(
-        `#__next > main > div.sc-aXZVg.fyChqS.tablescoresMobile > div > ul > div:nth-child(${i}) > div > div:nth-child(7)`
-      );
-      var scoreDraw = await page.evaluate(
-        (element) => element?.textContent,
-        Draw
-      );
-
-      var Lose = await page.waitForSelector(
-        `#__next > main > div.sc-aXZVg.fyChqS.tablescoresMobile > div > ul > div:nth-child(${i}) > div > div:nth-child(8)`
-      );
-      var scoreLose = await page.evaluate(
-        (element) => element?.textContent,
-        Lose
-      );
-
-      var Gd = await page.waitForSelector(
-        `#__next > main > div.sc-aXZVg.fyChqS.tablescoresMobile > div > ul > div:nth-child(${i}) > div > div.col-1.fixed-margin.text-point`
-      );
-      var scoreGd = await page.evaluate((element) => element?.textContent, Gd);
-
-      var pts = await page.waitForSelector(
-        `#__next > main > div.sc-aXZVg.fyChqS.tablescoresMobile > div > ul > div:nth-child(${i}) > div > div.col-1.fixed-margin.text-point`
-      );
-      var scorePts = await page.evaluate(
-        (element) => element?.textContent,
-        pts
-      );
-
-      response.push({
-        team: teamName,
-        pi: scorePI,
-        w: scoreWin,
-        d: scoreDraw,
-        l: scoreLose,
-        gd: scoreGd,
-        pts: scorePts,
-      });
-    }
+    response.push({
+      stock: stockValue,
+    });
     browser.close();
 
     return res.status(200).json({
       status: {
         code: 200,
         message: "success",
-        description: "get all score success",
+        description: "get stock status success",
       },
       data: response,
     });
@@ -85,8 +39,9 @@ export const getScore = async (req: Request, res: Response) => {
     return res.status(400).json({
       status: {
         code: 400,
-        message: e,
+        message: e.message,
         description: "Bad Request",
+        preview: url,
       },
       data: null,
     });
